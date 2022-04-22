f53bc6943b6656892f22c8c180642b61d7cca395
Din <dinmuhamed50@gmail.com>
2022-04-22 22:55:52 +0600

    Initial commit

---
+++ 5_2.py
@@ -0,0 +1,17 @@
+def odd_nums(nums):
+    some_num =(num for num in range(1, nums + 1, 2))
+    return some_num
+
+
+some = odd_nums(100)
+print(next(some))
+print(next(some))
+print(next(some))
+print(next(some))
+print(next(some))
+print(next(some))
+print(next(some))
+print(next(some))
+print(next(some))
+print(next(some))
+print(next(some))
