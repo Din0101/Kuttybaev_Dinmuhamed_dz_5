ae7ba7808eb4df13e86fa835a30c71087d41658b
Din <dinmuhamed50@gmail.com>
2022-04-22 22:55:52 +0600

    Initial commit

--- 5_5.py
+++ 5_5.py
@@ -1,7 +1,9 @@
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-result = []
-for i in src:
-    if src.count(i) == 1:
-        result.append(i)
-print(result)
+# result = []
+# for i in src:
+#     if src.count(i) == 1:
+#         result.append(i)
+# print(result)
 #как сделать пул реквест
+result = [i for i in src if src.count(i) == 1]
+print(result)
